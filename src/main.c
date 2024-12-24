34620e929fe5bd5cae98f6bd84cfa5b472040aa7
Romtry <romaindogcn@gmail.com>
2024-12-24 10:35:12 +0100

    deplacé lexer

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rothiery <rothiery@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 09:17:12 by rothiery          #+#    #+#             */
-/*   Updated: 2024/12/24 10:15:49 by rothiery         ###   ########.fr       */
+/*   Updated: 2024/12/24 10:19:54 by rothiery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,27 @@ void	print_error(unsigned int n)
 {
 	if (n == 0)
 		printf("");
+}
+
+void	lexer(t_token *token, char *input)
+{
+	unsigned int	i;
+	unsigned int	c;
+
+	i = 0;
+	c = 0;
+	token->word = malloc(sizeof(char *) * (count_word(input) + 1));
+	token->type = malloc(sizeof(t_type) * (count_word(input) + 1));
+	while (input[i] && is_sep(input[i]) == 0)
+		i++;
+	while (c < count_word(input))
+	{
+		i += malloc_word(token, input + i, c);
+		c++;
+	}
+	token->word[c] = NULL;
+	token->type[c] = '\0';
+	print_token(token);
 }
 
 int	main(void)
--- src/parsing.c
+++ src/parsing.c
@@ -6,32 +6,11 @@
 /*   By: rothiery <rothiery@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 09:17:06 by rothiery          #+#    #+#             */
-/*   Updated: 2024/12/24 10:11:09 by rothiery         ###   ########.fr       */
+/*   Updated: 2024/12/24 10:34:55 by rothiery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	lexer(t_token *token, char *input)
-{
-	unsigned int	i;
-	unsigned int	c;
-
-	i = 0;
-	c = 0;
-	token->word = malloc(sizeof(char *) * (count_word(input) + 1));
-	token->type = malloc(sizeof(t_type) * (count_word(input) + 1));
-	while (input[i] && is_sep(input[i]) == 0)
-		i++;
-	while (c < count_word(input))
-	{
-		i += malloc_word(token, input + i, c);
-		c++;
-	}
-	token->word[c] = NULL;
-	token->type[c] = '\0';
-	print_token(token);
-}
 
 void	parsing(t_token *token, unsigned int i)
 {
@@ -48,7 +27,9 @@ void	parsing(t_token *token, unsigned int i)
 			if(!ft_strcmp("echo", token->word[i]))
 			{
 				puts("echoed\n");
-				echo(token, i);
+				echo_pars();
+				// echo(token, i);
+				break;
 			}
 		}
 		i++;
