b2175f10a179c783ed67b495026470cd9798c253
Romtry <romaindogcn@gmail.com>
2025-02-25 14:56:48 +0100

    env dans le bon ordre après modif

--- src/exec/env_exec/sews.c
+++ src/exec/env_exec/sews.c
@@ -6,7 +6,7 @@
 /*   By: rothiery <rothiery@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 10:53:00 by rothiery          #+#    #+#             */
-/*   Updated: 2025/02/24 12:10:58 by rothiery         ###   ########.fr       */
+/*   Updated: 2025/02/25 11:35:36 by rothiery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,31 @@
 void sync_env_with_system(t_cmd *cmd)
 {
 	t_env *current;
-	int count = 0;
 	int i;
 
+	i = 0;
 	current = *get_env_head();
 	while (current)
 	{
-		count++;
+		i++;
 		current = current->next;
 	}
-	char **new_environ = malloc(sizeof(char *) * (count + 1));
+	char **new_environ = malloc(sizeof(char *) * (i + 1));
 	if (!new_environ)
 	{
 		perror("malloc");
 		return;
 	}
 	current = *get_env_head();
-	i = 0;
+	new_environ[i] = NULL;
+	i--;
 	while (current)
 	{
 		new_environ[i] = ft_strjoin(current->name, "=", false);
 		new_environ[i] = ft_strjoin(new_environ[i], current->value, true);
 		current = current->next;
-		i++;
+		i--;
 	}
-	new_environ[i] = NULL;
 	extern char **__environ;
 	__environ = new_environ;
 	if (*cmd->env_change == true)
