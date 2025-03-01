0dce1d81c9725731455517488cd9da2444aabf90
Rothiery <romaindogcn@gmail.com>
2025-03-01 10:42:24 +0100

    env_head normed

--- src/exec/env_exec/get_env_h.c
+++ src/exec/env_exec/get_env_h.c
@@ -6,106 +6,81 @@
 /*   By: rothiery <rothiery@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 10:35:49 by rothiery          #+#    #+#             */
-/*   Updated: 2025/02/27 09:53:19 by rothiery         ###   ########.fr       */
+/*   Updated: 2025/03/01 10:41:49 by rothiery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_env **get_env_head(void)
+t_env	*nodup(char *name, char *value)
 {
-    static t_env *env_head = NULL;
+	t_env	*new_var;
 
-    if (env_head == NULL)
-    {
-        extern char **__environ;
-        t_env   *new_var;
-        char    *equal_sign;
-        char    *name;
-        char    *value;
-        int     i;
-        int     duplicate_found;
-        t_env   *existing;
-
-        i = 0;
-        while (__environ[i])
-        {
-            equal_sign = ft_strchr(__environ[i], '=');
-            if (equal_sign)
-            {
-                name = ft_strndup(__environ[i], equal_sign - __environ[i]);
-                value = ft_strndup(equal_sign + 1, ft_strlen(equal_sign) - 1);
-            }
-            else
-            {
-                name = ft_strndup(__environ[i], ft_strlen(__environ[i]));
-                value = ft_strndup("", 0);
-            }
-            duplicate_found = 0;
-            existing = env_head;
-            while (existing)
-            {
-                if (ft_strcmp(existing->name, name) == 0)
-                {
-                    free(name);
-                    free(value);
-                    duplicate_found = 1;
-                    break;
-                }
-                existing = existing->next;
-            }
-            if (duplicate_found == 0)
-            {
-                new_var = malloc(sizeof(t_env));
-                if (!new_var)
-                {
-                    perror("malloc");
-                    free(name);
-                    free(value);
-                    return (NULL);
-                }
-                new_var->name = name;
-                new_var->value = value;
-                new_var->next = env_head;
-                env_head = new_var;
-            }
-            i++;
-        }
-    }
-    return (&env_head);
+	new_var = malloc(sizeof(t_env));
+	if (!new_var)
+	{
+		perror("malloc");
+		free(name);
+		free(value);
+		return (NULL);
+	}
+	new_var->name = name;
+	new_var->value = value;
+	return (new_var);
 }
 
-
-
-void remove_duplicates_from_env_head(void)
+t_env	*existing(char *name, char *value, t_env *env_head)
 {
-    t_env *current = *get_env_head();
-    t_env *prev = NULL;
-    t_env *temp;
+	t_env	*existing;
+	t_env	*new_var;
 
-    while (current)
-    {
-        temp = *get_env_head();
-        while (temp != current)
-        {
-            if (ft_strcmp(temp->name, current->name) == 0)
-            {
-                if (prev)
-                    prev->next = current->next;
-                else
-                    *get_env_head() = current->next;
-                free(current->name);
-                free(current->value);
-                free(current);
-                current = prev ? prev->next : *get_env_head();
-                break;
-            }
-            temp = temp->next;
-        }
-        if (temp == current)
-        {
-            prev = current;
-            current = current->next;
-        }
-    }
+	existing = env_head;
+	while (existing)
+	{
+		if (ft_strcmp(existing->name, name) == 0)
+		{
+			free(name);
+			free(value);
+			break ;
+		}
+		existing = existing->next;
+	}
+	if (!existing)
+	{
+		new_var = nodup(name, value);
+		if (new_var == NULL)
+			return (NULL);
+		new_var->next = env_head;
+		return (new_var);
+	}
+	return (env_head);
+}
+
+t_env	**get_env_head(void)
+{
+	static t_env	*env_head = NULL;
+	extern char		**__environ;
+	char			*name;
+	char			*value;
+	int				i;
+
+	i = -1;
+	while (__environ[++i])
+	{
+		value = ft_strchr(__environ[i], '=');
+		if (value)
+		{
+			name = ft_strndup(__environ[i], value - __environ[i]);
+			value = ft_strndup(value + 1, ft_strlen(value) - 1);
+		}
+		else
+		{
+			name = ft_strndup(__environ[i], ft_strlen(__environ[i]));
+			value = ft_strndup("", 0);
+		}
+		env_head = existing(name, value, env_head);
+		if (env_head == NULL)
+			return (NULL);
+	}
+	return (&env_head);
 }
