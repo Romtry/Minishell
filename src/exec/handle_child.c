56975103cd598f46d094ec092ad5793d367ce68c
Romtry <romaindogcn@gmail.com>
2025-03-16 15:42:11 +0100

    Revert "fixed"
    This reverts commit 68a358b71d5a8e794ca8b7b2c53d28b18831d986.

--- src/exec/handle_child.c
+++ src/exec/handle_child.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_child.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttouahmi <ttouahmi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rothiery <rothiery@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 13:14:27 by rothiery          #+#    #+#             */
-/*   Updated: 2025/03/16 15:11:15 by ttouahmi         ###   ########.fr       */
+/*   Updated: 2025/03/11 08:25:35 by rothiery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,8 +96,8 @@ void	handle_child(int i, t_cmd *cmd, int input_fd, int pipe_fd[2])
 	tmp_cmd = cmd_cpy(cmd, i);
 	count = cmd_count(cmd);
 	is_not_last = (i < count - 1);
-	if (input_fd != STDIN_FILENO)
-		dup2(input_fd, STDIN_FILENO);
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
 	if (is_not_last)
 	{
 		if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
@@ -113,7 +113,11 @@ void	handle_child(int i, t_cmd *cmd, int input_fd, int pipe_fd[2])
 		*cmd->exit_stat = EXIT_FAILURE;
 		return ;
 	}
-	else
-		execute_command2(&tmp_cmd, cmd);
+	if (input_fd != STDIN_FILENO)
+	{
+		dup2(input_fd, STDIN_FILENO);
+		close(input_fd);
+	}
+	execute_command2(&tmp_cmd, cmd);
 	exit(*cmd->exit_stat);
 }
